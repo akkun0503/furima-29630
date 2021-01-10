f98b35f9168204b021e7a1d499480dee3cf22f91
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-01-10 17:19:17 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_fee_status
 end
