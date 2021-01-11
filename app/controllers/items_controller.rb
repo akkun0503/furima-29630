5d0e12728dd40b8e8b9515414d8ba58717f2796e
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-01-11 12:45:45 +0900

    Itemモデルのバリデーション修正、非ログイン時の商品出品制限機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @item = Item.all
   end
@@ -18,7 +20,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,16 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :info, length: { maximum: 1000 }
-    validates :price, numericality: { grater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-      with_options numericality: { other_than: 1 } do
-        validates :category_id
-        validates :sales_status_id
-        validates :shipping_fee_status_id
-        validates :prefecture_id
-        validates :scheduled_delivery_id
-      end
+    validates :price, numericality: { only_integer: true, message: "Half-width number" }
+  end
+  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" } 
+
+  with_options numericality: { other_than: 1, message: "Select" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
   end
 end
