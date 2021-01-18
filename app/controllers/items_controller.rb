28081ec1291e194ac7f5f6712286478945a7f669
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-01-18 09:16:25 +0900

    商品編集画面へのリンク設定、editアクション定義、商品編集画面用のビューファイル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
