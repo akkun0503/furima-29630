ab6d32f225470362107b1466474dd4083b4bb988
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-01-22 08:42:16 +0900

    可読性向上のための修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :baria_item, only: [:edit, :update]
   before_action :find_item, only: [:show, :edit, :update]
+  before_action :baria_item, only: [:edit, :update]
 
   def index
     @items = Item.all.order("id DESC").includes(:user)
@@ -39,14 +39,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def baria_item
-    unless Item.find(params[:id]).user.id.to_i == current_user.id
-      redirect_to item items_path(current_user)
-    end
-  end
-
   def find_item
     @item = Item.find(params[:id])
+  end
+
+  def baria_item
+    if @item.user.id.to_i != current_user.id
+      redirect_to item items_path(current_user)
+    end
   end
 
 end
