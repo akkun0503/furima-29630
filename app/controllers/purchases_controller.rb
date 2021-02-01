33721efd95ce8c1bab4bf3195f690034fabf304b
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-02-01 10:59:17 +0900

    フォームの実装及びフォームの情報をテーブルに保存する機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,26 @@
 class PurchasesController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+
   def index
     @purchase_item = PurchaseItem.new
   end
-  
+
   def create
+    @purchase_item = PurchaseItem.new(purchase_item_params)
+    if @purchase_item.valid?
+      @purchase_item.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_item_params
+    params.require(:purchase_item).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -4,14 +4,15 @@ class PurchaseItem
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
-    validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
   end
 
+
   def save
-    purchase = Purchase.create(user_id: current_user.id, item_id: @item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
