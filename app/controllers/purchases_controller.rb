5fc4d282e4c19743141e21bab6d7a6f1d2374f23
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-02-02 19:52:01 +0900

    サーバーサイドでtokenを扱えるようにするためのコントローラー、モデルの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -19,7 +19,7 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_item_params
-    params.require(:purchase_item).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_item).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def find_item
