745234c58d7c2eedd45b4fa0341c4c9409af8429
akkun0503 <atsushi.fujiwara.05.03@gmail.com>
2021-02-02 12:47:40 +0900

    カード情報を削除する記述の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,14 +18,14 @@ const pay = () => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("purchase_item[order[number]]").removeAttribute("name");
-      document.getElementById("purchase_item[order[exp_month]]").removeAttribute("name");
-      document.getElementById("purchase_item[order[exp_year]]").removeAttribute("name");
-      document.getElementById("purchase_item[order[cvc]]").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
